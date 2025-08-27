ca82ca616e2d81158c88598d4d2b758d5d9b674c
GiovanaFerreira20 <270983@unifio.edu.br>
2025-08-27 23:33:46 +0000

    Projeto atualizado

---
+++ quarkus-projeto/src/main/java/entidades/Venda.java
@@ -0,0 +1,31 @@
+package entidades;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.annotation.processing.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer codigo;
+
+    public LocalDateTime horario;
+
+    public Integer valorTotal;
+
+    public Integer quantidadeTotal;
+
+    @ManyToOne
+    public Cliente cliente;
+
+    @ManyToOne
+    public Funcionario funcionario;
+
+}
